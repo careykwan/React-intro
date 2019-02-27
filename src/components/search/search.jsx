0aa2cddca04a45aa6d8d2c6eadd303cea2b9105d
Training User <train@trainpc-09.dynamic.wgtn.cat-it.co.nz>
2019-02-28 11:47:34 +1300

    Intro to React

---
+++ src/components/search/search.jsx
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchFieldValue: ''
+    }
+
+    this.searchFieldChange = this.searchFieldChange.bind(this);
+  }
+
+  searchFieldChange(event) {
+    this.setState({
+      searchFieldValue: event.target.value
+    })
+    this.props.searchValueEntered(event.target.value);
+  }
+
+  render () {
+    return (
+      <div className="mb-3">
+        <label>
+          Search:
+          <input
+            className="ml-2"
+            type="search"
+            autoComplete="off"
+            onChange={this.searchFieldChange}
+            value={this.state.searchFieldValue}
+          />
+        </label>
+      </div>
+    );
+  }
+}
+
+Search.propTypes = {
+  searchValueEntered: PropTypes.func.isRequired,
+  searchFieldValue: PropTypes.string
+}
+export default Search;
